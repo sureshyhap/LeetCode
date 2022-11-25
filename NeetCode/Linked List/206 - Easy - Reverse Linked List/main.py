df6654b0ac5cc5b0ec43b4dd5d8f14d5aa423cee
Suresh Yhap <sureshyhap@gmail.com>
2022-11-25 17:27:35 -0500

    Finished Reverse List

--- NeetCode/Linked List/206 - Easy - Reverse Linked List/main.py
+++ NeetCode/Linked List/206 - Easy - Reverse Linked List/main.py
@@ -5,6 +5,25 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return
+        if not head.next:
+            return head
+        node_vals = []
+        current = head
+        while current:
+            node_vals.append(current.val)
+            current = current.next
+        result = ListNode()
+        current = result
+        while len(node_vals) > 1:
+            current.val = node_vals.pop()
+            current.next = ListNode()
+            current = current.next
+        current.val = node_vals.pop()
+        return result
+
+"""
         if head:
             lyst = []
             current = head
@@ -19,3 +38,17 @@ class Solution:
                     current.next = ListNode()
                 current = current.next
             return reversed_list
+"""
+
+"""
+        reversed_list = None
+        if not head:
+            return None
+        def revList(head_node):
+            nonlocal reversed_list
+            reversed_list = ListNode(head_node.val, reversed_list)
+            if head_node.next:
+                revList(head_node.next)
+        revList(head)
+        return reversed_list
+"""
